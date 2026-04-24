41f95a2c13cb46d6b417e812285c5ed9e44e401a
Lsh6831 <rltmsla5@naver.com>
2026-04-24 18:36:42 +0900

    fix : Main.cpp에
    #include <cstdlib>
    #include <ctime>
    
        srand((unsigned int)time(NULL));
    
        추가하여 랜덤값을 제대로 불러올수 있도록 설정

--- MiniPocketMon/Main.cpp
+++ MiniPocketMon/Main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "Monster.h"
 using namespace std;
-
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
+    srand((unsigned int)time(NULL));
+    
     Monster monster(10,10,10,10,1,1);
     cout<<"hi";
     
